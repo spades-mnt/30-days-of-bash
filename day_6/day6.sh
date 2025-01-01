3f838d4f107b79a2164984b835719eba56b71870
spades_mnt <michael@gmail.com>
2025-01-01 12:44:32 +0000

    bash functions

---
+++ day_6/day6.sh
@@ -0,0 +1,10 @@
+ #!/bin/bash
+
+#bash functions
+
+about_me () {
+
+     read -p "my name is Michael nuertey. i am 28 yrs and currently pursuing cloud engineering with altshool africa. i am glad to say i am enjoyingthis journey and i cant wait to call myself a cloud engineer".
+}
+
+about_me
