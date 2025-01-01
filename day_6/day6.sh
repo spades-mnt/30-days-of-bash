a69b7b948395332f3114621e004bca4277f73502
spades_mnt <michael@gmail.com>
2025-01-01 12:46:51 +0000

    a couple of changes

--- day_6/day6.sh
+++ day_6/day6.sh
@@ -4,7 +4,7 @@
 
 about_me () {
 
-     read -p "my name is Michael nuertey. i am 28 yrs and currently pursuing cloud engineering with altshool africa. i am glad to say i am enjoyingthis journey and i cant wait to call myself a cloud engineer".
+     read -p "my name is Michael nuertey. i am 28 yrs and currently pursuing cloud engineering with altshool africa. i am glad to say i am enjoying this journey and i cant wait to call myself a cloud engineer".
 }
 
 about_me
